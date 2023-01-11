f8ac22c935ea0ce50fb48e2d37dc1d44ffc4de6f
Omniscient <omniscient.oce@gmail.com>
2023-01-12 10:34:48 +1100

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require('wezterm')
+return {
+  font = wezterm.font('MesloLGM Nerd Font Mono'),
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    top = 2,
+    left = 2,
+    right = 0,
+    bottom = 0,
+  },
+}
